f3a77f3513d5e4988cbabf63c49c7f480af31de0
YuriSolomon <juristein21@gmail.com>
2025-03-26 11:19:47 +0100

    change between x and o and lift state up

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,16 +1,34 @@
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ];
 
-export default function GameBoard() {
+export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectedSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => { // update an inmutable to avoide state schedueling issues
+            const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])]; // copy array and nexted arrays
+            updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol; // update only the new arrays
+            return updatedGameBoard;
+        });
+
+        onSelectSquare();
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
-                        {row.map((cell, cellIndex) => <li key={cellIndex}><button>{cell}</button></li>)}
+                        {row.map((cell, colIndex) => (
+                            <li key={colIndex}>
+                                <button onClick={() => handleSelectedSquare(rowIndex, colIndex)}>{cell}</button>
+                            </li>
+                        ))}
                     </ol>
                 </li>
             ))}
