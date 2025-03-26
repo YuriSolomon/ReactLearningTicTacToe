981b29c4078d13c02a380e41fb47b280182ccb53
YuriSolomon <juristein21@gmail.com>
2025-03-26 17:20:37 +0100

    polish and derive

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import Log from "./components/Log";
 import GameOver from "./components/GameOver";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2'
+}
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
@@ -14,23 +19,14 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer
 }
 
-const initialGameBoard = [
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
 ];
 
-function App() {
-  const [players, setPlayers] = useState({
-    X: 'Player 1',
-    O: 'Player 2'
-  });
-  // const [activePlayer, setActivePlayer] = useState('X');
-  const [gameTurns, setGameTurns] = useState([]);
-
-  const activePlayer = deriveActivePlayer(gameTurns);
-
-  let gameBoard = [...initialGameBoard.map(array => [...array])]; // allows to empty gameBoard when emptying gameTurns
+function deriveGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])]; // allows to empty gameBoard when emptying gameTurns
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -38,7 +34,10 @@ function App() {
 
     gameBoard[row][col] = player;
   }
+  return gameBoard;
+}
 
+function deriveWinner(gameBoard, players) {
   let winner;
 
   for (const combination of WINNING_COMBINATIONS) {
@@ -53,10 +52,19 @@ function App() {
     }
   }
 
+  return winner;
+}
+
+function App() {
+  const [players, setPlayers] = useState(PLAYERS);
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns)
+  const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectedSquare(rowIndex, colIndex) {
-    // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns(prevTurns => {
       const currentPlayer = deriveActivePlayer(prevTurns);
 
@@ -86,8 +94,18 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'} onChangeName={handlePlayerNameChange} />
-          <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'} onChangeName={handlePlayerNameChange} />
+          <Player
+            initialName={PLAYERS.X}
+            symbol='X'
+            isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player
+            initialName={PLAYERS.O}
+            symbol='O'
+            isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}
+          />
         </ol>
         {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
         <GameBoard onSelectSquare={handleSelectedSquare} board={gameBoard} />
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,19 +1,4 @@
-// import { useState } from "react";
-
 export default function GameBoard({ onSelectSquare, board }) {
-
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    // function handleSelectedSquare(rowIndex, colIndex) {
-    //     setGameBoard((prevGameBoard) => { // update an inmutable to avoide state schedueling issues
-    //         const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])]; // copy array and nexted arrays
-    //         updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol; // update only the new arrays
-    //         return updatedGameBoard;
-    //     });
-
-    //     onSelectSquare();
-    // }
-
     return (
         <ol id="game-board">
             {board.map((row, rowIndex) => (
