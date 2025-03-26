626ffb5cf0ff7c5ae1ede53f502b5138b64e137f
YuriSolomon <juristein21@gmail.com>
2025-03-26 17:00:54 +0100

    use name of winner instead of symbol

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react"
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
     const [isEditing, setIsEditing] = useState(false);
     const [playerName, setPlayerName] = useState(initialName);
     function handleEditClick() {
         setIsEditing(editing => !editing); //use function and not directly !isEditing to prevent schedualing of state update
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
     }
 
     let editablePlayerName = <span className="player-name">{playerName}</span>;
