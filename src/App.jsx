c32fbe2c004cda672328963af04b7a3889198e7a
YuriSolomon <juristein21@gmail.com>
2025-03-26 12:05:31 +0100

    enable logs

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectedSquare} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   )
 }
